0f085fad023d01fc08712c2cc59a72f2e7e61918
Luis Eduardo <eduvoices25@gmail.com>
2023-10-13 17:06:24 -0300

    limpando arquivos na pasta src

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,11 +1,7 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import App from './App';
-import { Access } from './pages/Access';
-import { Error } from './pages/Error';
 import { Login } from './pages/Login';
-import { NotFound } from './pages/NotFound';
-import { Wizard } from './pages/Wizard';
 
 const AppWrapper = () => {
     let location = useLocation();
@@ -17,10 +13,6 @@ const AppWrapper = () => {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/error" element={<Error />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/access" element={<Access />} />
-            <Route path="/wizard" element={<Wizard />} />
             <Route path="*" element={<App />} />
         </Routes>
     );
