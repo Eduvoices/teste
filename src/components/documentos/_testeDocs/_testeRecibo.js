55b0cf5d4189b0c50121f75069a054b1c63aec10
Luís <eduvoices25@gmail.com>
2023-12-05 17:07:10 -0300

    testando a troca de parâmetros

--- src/components/documentos/_testeDocs/_testeRecibo.js
+++ src/components/documentos/_testeDocs/_testeRecibo.js
@@ -1,17 +1,26 @@
 import React from "react"
-import logo from '../../../assets/logo.png'
+import logo1 from '../../../assets/logo.png'
+import logo2 from '../../../assets/logo-black-alt.png'
 
-const Recibo1 = ({numeroCtrl, nome, cash, extenso, emitente, dataAtual, sobrenome}) => {
+const Recibo1 = ({numeroCtrl, nome, cash, extenso, emitente, dataAtual, sobrenome, logo}) => {
+    const imgLogo = () => {
+        if (logo === 'tecjus') {
+            return logo1
+        } else if (logo === 'babylon') {
+            return logo2
+        }
+    }
+
     return (
         <div>
-        <img src={logo} alt="logo"/>
+        <img src={imgLogo()} alt="logo"/>
         <h1>RECIBO</h1>
         <div id="controle">
             <span>{numeroCtrl}</span>
         </div>
         <p>Recebemos de <span id="recibo_nome">{nome} {sobrenome}</span> a importância de <span id="recibo_valor">R$ {cash}</span> ({extenso} reais), referente ao pagamento de honorários advocatícios.</p>
         <span id="data">{dataAtual}</span>
-        <span id="assinatura">{emitente} {sobrenome}</span>
+        <span id="assinatura">{emitente}</span>
 
         <div className="canhoto">
             <span>{dataAtual}</span>
--- src/pages/Recibos.js
+++ src/pages/Recibos.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Dropdown } from 'primereact/dropdown';
+// import { InputText } from 'primereact/inputtext';
+// import { Dropdown } from 'primereact/dropdown';
 
 import Recibo1 from '../components/documentos/_testeDocs/_testeRecibo';
-import Recibo2 from '../components/documentos/_teste2Docs/_teste2Recibo';
-import Procuração from '../components/documentos/_testeDocs/_testeProcuração';
-import Procuração2 from '../components/documentos/_teste2Docs/_teste2Procuração';
-import ReciboAvalon from '../components/documentos/_teste4Docs/_testeRecibo4';
-import Procuração4 from '../components/documentos/_teste4Docs/_testeProcuração4';
-import ReciboRoma from '../components/documentos/_testeRecibo3.js/_teste3Recibo';
-import ProcuraçãoRoma from '../components/documentos/_testeRecibo3.js/_testeProcuração3';
+// import Recibo2 from '../components/documentos/_teste2Docs/_teste2Recibo';
+// import Procuração from '../components/documentos/_testeDocs/_testeProcuração';
+// import Procuração2 from '../components/documentos/_teste2Docs/_teste2Procuração';
+// import ReciboAvalon from '../components/documentos/_teste4Docs/_testeRecibo4';
+// import Procuração4 from '../components/documentos/_teste4Docs/_testeProcuração4';
+// import ReciboRoma from '../components/documentos/_testeRecibo3.js/_teste3Recibo';
+// import ProcuraçãoRoma from '../components/documentos/_testeRecibo3.js/_testeProcuração3';
 
 const Recibos = (props) => {
 const [nome, setNome] = useState('')
     const [emitente, setEmitente] = useState('')
     const [valor, setValor] = useState('')
     const [extenso, setExtenso] = useState('')
-    const [doc, setDoc] = useState('')
-    const [param, setParam] = useState('Escritório 1')
+    // const [doc, setDoc] = useState('')
+    // const [param, setParam] = useState('Escritório 1')
     const [sobrenome, setSobrenome] = useState('')
     const [resposta, setResposta] = useState('')
+    const [logo, setLogo] = useState('tecjus')
 
     function dataAtual() {
         let data = new Date()
@@ -29,49 +30,49 @@ const [nome, setNome] = useState('')
         return `${day}/${month}/${data.getFullYear()}`
     }
 
-    const dropdownValues = [
-        {name: 'Recibo'},
-        {name: 'Procuração'},
-    ]
+    // const dropdownValues = [
+    //     {name: 'Recibo'},
+    //     {name: 'Procuração'},
+    // ]
 
-    const dropdownEscritorios = [
-        {name: 'Escritório 1'},
-        {name: 'Escritório 2'},
-        {name: 'Escritório 3'},
-        {name: 'Escritório 4'}
-    ]
+    // const dropdownEscritorios = [
+    //     {name: 'Escritório 1'},
+    //     {name: 'Escritório 2'},
+    //     {name: 'Escritório 3'},
+    //     {name: 'Escritório 4'}
+    // ]
 
-    function handleInputChange(e) {
-        e.preventDefault()
-        const {value} = e.target
-        setDoc(value)
-    }
+    // function handleInputChange(e) {
+    //     e.preventDefault()
+    //     const {value} = e.target
+    //     setDoc(value)
+    // }
 
-    function handleOfficeChange(e) {
-        e.preventDefault()
-        const {value} = e.target
-        setParam(value)
-    }
+    // function handleOfficeChange(e) {
+    //     e.preventDefault()
+    //     const {value} = e.target
+    //     setParam(value)
+    // }
 
-    function returnDoc() {
-        if (doc.name === 'Recibo' && param.name === 'Escritório 1') {
-            return <Recibo1 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
-        } else if (doc.name === 'Recibo' && param.name === 'Escritório 2') {
-            return <Recibo2 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
-        } else if (doc.name === 'Procuração' && param.name === 'Escritório 1') {
-            return <Procuração dataAtual={dataAtual()} outorgante={emitente}/>
-        } else if (doc.name === 'Procuração' && param.name === 'Escritório 2') {
-            return <Procuração2 dataAtual={dataAtual()} outorgante={emitente}/>
-        } else if (doc.name === 'Recibo' && param.name === 'Escritório 4') {
-            return <ReciboAvalon nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
-        } else if (doc.name === 'Procuração'&& param.name === 'Escritório 4') {
-            return <Procuração4 dataAtual={dataAtual()} outorgante={emitente}/>
-        } else if (doc.name === 'Recibo' && param.name === 'Escritório 3') {
-            return <ReciboRoma nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
-        } else if (doc.name === 'Procuração' && param.name === 'Escritório 3') {
-            return <ProcuraçãoRoma dataAtual={dataAtual()} outorgante={emitente}/>
-        }
-    }
+    // function returnDoc() {
+    //     if (doc.name === 'Recibo' && param.name === 'Escritório 1') {
+    //         return <Recibo1 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
+    //     } else if (doc.name === 'Recibo' && param.name === 'Escritório 2') {
+    //         return <Recibo2 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
+    //     } else if (doc.name === 'Procuração' && param.name === 'Escritório 1') {
+    //         return <Procuração dataAtual={dataAtual()} outorgante={emitente}/>
+    //     } else if (doc.name === 'Procuração' && param.name === 'Escritório 2') {
+    //         return <Procuração2 dataAtual={dataAtual()} outorgante={emitente}/>
+    //     } else if (doc.name === 'Recibo' && param.name === 'Escritório 4') {
+    //         return <ReciboAvalon nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
+    //     } else if (doc.name === 'Procuração'&& param.name === 'Escritório 4') {
+    //         return <Procuração4 dataAtual={dataAtual()} outorgante={emitente}/>
+    //     } else if (doc.name === 'Recibo' && param.name === 'Escritório 3') {
+    //         return <ReciboRoma nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()}/>
+    //     } else if (doc.name === 'Procuração' && param.name === 'Escritório 3') {
+    //         return <ProcuraçãoRoma dataAtual={dataAtual()} outorgante={emitente}/>
+    //     }
+    // }
 
     const cash = parseFloat(valor).toLocaleString('pt-br', {minimumFractionDigits: 2})
 
@@ -83,9 +84,6 @@ const [nome, setNome] = useState('')
                         if (res[1] === undefined) {
                             console.log('No params')
                         } else {
-
-                            console.log(res[1])
-
                             var param = res[1].split('&')
                             setEmitente(param[0])
                             setNome(param[1])
@@ -93,10 +91,9 @@ const [nome, setNome] = useState('')
                             setExtenso(param[3])
                             setValor(param[4])
                             setResposta(param[5])
+                            setLogo(param[6])
                         }
     },[])
-
-    console.log(nome, emitente)
 
     return (
         <>
@@ -159,7 +156,7 @@ const [nome, setNome] = useState('')
             <div>
                 <div className='card'>
                     <div id="invoice-content">
-                        <Recibo1 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()} sobrenome={sobrenome} numeroCtrl={resposta}/>
+                        <Recibo1 nome={nome} cash={cash} extenso={extenso} emitente={emitente} dataAtual={dataAtual()} sobrenome={sobrenome} numeroCtrl={resposta} logo={logo}/>
                     </div>
                 </div>
             </div>
